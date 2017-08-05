05b7feb1530226cf6944dda4122cb829d57c5b4e
Pablo <pablosls@gmail.com>
2017-08-05 14:20:25 -0300

    Add project

---
+++ JenkinsWebProject/JenkinsWebProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JenkinsWebProject.MvcApplication" Language="C#" %>
---
+++ JenkinsWebProject/JenkinsWebProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JenkinsWebProject.Startup))]
+namespace JenkinsWebProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
